8b7deb394aa5a7a52e2f3ba698cd6fe668c1bfd3
Leo Y. Li <leo.yuxiu.li@gmail.com>
2017-09-08 14:40:07 -0600

    Efficiency improvement
    Reduced undesired steps being exec.

--- microwave-transition-frequency-loop-permutator.py
+++ microwave-transition-frequency-loop-permutator.py
@@ -1,11 +1,9 @@
 # Author    :  Leo Y. Li
 # Release   :  2017/09/08
-# Version   :  1.0.0
-
-
+# Version   :  1.0.1
 
 def checkLoop(inputList, deviations=0.03, minimumSeparations=0, rounded=True, instantOutput=True, reportRates=50):
-    # option insurance
+    # Insured options
     deviations = abs(deviations)
     reportRates = abs(reportRates)
     minimumSeparations = abs(minimumSeparations)
@@ -30,22 +28,21 @@ def checkLoop(inputList, deviations=0.03, minimumSeparations=0, rounded=True, in
             print("calculating... currently at", A, "-th line in the sequence.")
 
         for B in range(A + 1, length):
+            if (inputList[B] - inputList[A]) < minimumSeparations:
+                continue
+
             for C in range(B + 1, length):
+                if (inputList[C] - inputList[B]) < minimumSeparations:
+                    continue
+
                 for D in range(C + 1, length):
-
-                    # check options: minimum separations between any two lines (for time saving)
-                    if minimumSeparations > 0:
-                        separations = [inputList[B] - inputList[A], inputList[C] - inputList[B],
-                                       inputList[D] - inputList[C]]
-
-                        # if meet the threshold, skip the rest of calculations
-                        if True in list(map(lambda i: i < minimumSeparations, separations)):
-                            break
+                    if (inputList[D] - inputList[C]) < minimumSeparations:
+                        continue
 
                     # check criteria for a loop
-                    errors = abs(inputList[A] - inputList[B] - inputList[C] + inputList[D])
+                    errors = round(abs(inputList[D] - inputList[C] - inputList[B] + inputList[A]), 4)
                     if errors <= deviations:
-                        newFoundLoop = [inputList[A], inputList[B], inputList[C], inputList[D], round(errors, 3)]
+                        newFoundLoop = [inputList[D], inputList[C], inputList[B], inputList[A], errors]
                         recordBook.append(newFoundLoop)
                         if instantOutput:
                             print(newFoundLoop)
